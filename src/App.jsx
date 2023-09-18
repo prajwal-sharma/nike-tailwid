d404cb2f20b6df0e9d0214786ab0cdf7ce8e872e
Prajwal Sharma <prajwal.sharma@novoinvent.com>
2023-09-18 14:42:58 +0530

    added nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import {CustomerReviews,Footer,Hero,PopularProduct,Services,SpecialOffer,Subscribe,SuperQuality} from './sections'
+import Nav from './components/Nav';
 function App() {
   return (
     <main className="relative">
-      {/* <Nav/> */}
+      <Nav/>
       <section className="xl:padding-1 wide:padding-r padding-b">
         <Hero/>
       </section>
@@ -19,7 +20,7 @@ function App() {
         <SpecialOffer/>
       </section>
       <section className="bg-pale-blue padding">
-        Customer review
+        <CustomerReviews/>
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
         <Subscribe/>   
